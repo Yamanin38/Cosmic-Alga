9331cb5ce896eb2c978b8ebff3237b2267b0e98a
B8138@oic.osaka.jp <B8138@oic.osaka.jp>
2023-04-27 08:48:25 +0900

    弾発射用のスクリプト、弾移動用のスクリプト、弾の位置取得用のオブジェクト作成

---
+++ Cosmic-Alga/Assets/Resouce/bullet/bullet.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+
+    public float fMoveSpeed = 7.0f;     // �ړ��l
+    public GameObject BulletObj;
+    public Vector3 bulletPoint;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletPoint = transform.Find("BulletPoint").localPosition;;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("down"))
+        {
+            
+            Instantiate(BulletObj, bulletPoint, Quaternion.identity);
+        }
+
+    }
+}
